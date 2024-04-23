189ab4686c0c72f542fef6eef6b553d900387e34
mrbananaaa <personal.astagiriii@gmail.com>
2024-04-23 21:12:07 +0700

    update vercel
    reorder config
    
    add eslint plugin
    
    adding and configure eslint-plugin-tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,29 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules/**"],
+  parser: "@typescript-eslint/parser",
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.js"],
+      parser: "@typescript-eslint/parser",
+      rules: {
+        // tailwindcss
+        "tailwindcss/no-custom-classname": "off",
+      },
+    },
+  ],
+};

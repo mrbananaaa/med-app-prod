d3c41405dc82810c08027388068cec87a9ddcc48
mrbananaaa <personal.astagiriii@gmail.com>
2024-04-24 17:05:02 +0700

    setup prettier and eslint
    test linter and formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,28 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules/**"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules/**', '*.config.*'],
+  parser: '@typescript-eslint/parser',
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:tailwindcss/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: ["react-refresh"],
+  plugins: ['react-refresh', 'prettier', 'tailwindcss'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
   overrides: [
     {
-      files: ["*.ts", "*.tsx", "*.js"],
-      parser: "@typescript-eslint/parser",
+      files: ['*.ts', '*.tsx', '*.js'],
+      parser: '@typescript-eslint/parser',
       rules: {
         // tailwindcss
-        "tailwindcss/no-custom-classname": "off",
+        'tailwindcss/no-custom-classname': 'off',
+
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
       },
     },
   ],

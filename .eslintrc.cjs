f30ef952f3da64a8baca2d9f5bda3ca9f6db8444
mrbananaaa <personal.astagiriii@gmail.com>
2024-04-25 15:16:12 +0700

    add more linter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,22 +5,64 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:tailwindcss/recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['react-refresh', 'prettier', 'tailwindcss'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-  },
+  plugins: ['react-refresh', 'import', 'prettier', 'tailwindcss'],
   overrides: [
     {
       files: ['*.ts', '*.tsx', '*.js'],
       parser: '@typescript-eslint/parser',
+      settings: {
+        react: { version: 'detect' },
+        'import/resolver': {
+          typescript: {},
+        },
+      },
       rules: {
+        // react-refresh
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
         // tailwindcss
         'tailwindcss/no-custom-classname': 'off',
+
+        //import
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: ['@/features/*/*'],
+          },
+        ],
+
+        'linebreak-style': ['error', 'unix'],
+        'react/prop-types': 'off',
+
+        'import/order': [
+          'error',
+          {
+            groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+            'newlines-between': 'always',
+            alphabetize: { order: 'asc', caseInsensitive: true },
+          },
+        ],
+        'import/default': 'off',
+        'import/no-named-as-default-member': 'off',
+        'import/no-named-as-default': 'off',
+        // 'import/no-unresolved': 'error',
+
+        'react/react-in-jsx-scope': 'off',
+
+        '@typescript-eslint/no-unused-vars': ['error'],
+
+        '@typescript-eslint/explicit-function-return-type': ['off'],
+        '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        '@typescript-eslint/no-empty-function': ['off'],
+        '@typescript-eslint/no-explicit-any': ['off'],
 
         'prettier/prettier': ['error', {}, { usePrettierrc: true }],
       },

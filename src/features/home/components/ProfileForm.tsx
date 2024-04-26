d7fe032cc4ef1e9bae58bc3a8f06a92a754a482a
mrbananaaa <personal.astagiriii@gmail.com>
2024-04-26 23:05:31 +0700

    refactoring component

--- src/features/home/components/ProfileForm.tsx
+++ src/features/home/components/ProfileForm.tsx
@@ -2,6 +2,13 @@ import type { UseFormReturn } from 'react-hook-form';
 
 import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 import type { TFormSchema } from '../types';
 
@@ -9,32 +16,98 @@ export const ProfileForm = (profileForm: UseFormReturn<TFormSchema, any, undefin
   return (
     <Form {...profileForm}>
       <form>
-        {(
-          [
-            ['nama', 'Nama', 'Nama Lengkap'],
-            ['nik', 'NIK', '*305050xxxxxxxx'],
-            ['ttl', 'TTL', 'Surabaya, 30 Februari 1943'],
-            ['noRm', 'No. RM', '*00.20.xx - 99.xx.xx'],
-          ] as const
-        ).map(([name, label, placeholder]) => (
-          <FormField
-            key={name}
-            control={profileForm.control}
-            name={name}
-            render={({ field }) => (
-              <FormItem className="mt-4">
-                <FormLabel className="font-semibold">{label}</FormLabel>
-                <FormControl>
-                  <Input
-                    className="px-4 py-6 outline outline-2 outline-blue-500 placeholder:text-[#898989]"
-                    placeholder={placeholder}
-                    {...field}
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-        ))}
+        <FormField
+          control={profileForm.control}
+          name="nama"
+          render={({ field }) => (
+            <FormItem className="mt-4">
+              <FormLabel className="font-semibold">Nama</FormLabel>
+              <FormControl>
+                <Input
+                  className="px-4 py-6 outline outline-2 outline-blue-500 placeholder:text-[#898989]"
+                  placeholder="Nama Lengkap"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={profileForm.control}
+          name="nik"
+          render={({ field }) => (
+            <FormItem className="mt-4">
+              <FormLabel className="font-semibold">NIK</FormLabel>
+              <FormControl>
+                <Input
+                  className="px-4 py-6 outline outline-2 outline-blue-500 placeholder:text-[#898989]"
+                  placeholder="*305050xxxxxxxx"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <div className="mt-4">
+          <FormLabel className="font-semibold">TTL</FormLabel>
+          <div className="mt-2 flex items-center space-x-4">
+            <FormField
+              control={profileForm.control}
+              name="tempatLahir"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="px-4 py-6 outline outline-2 outline-blue-500">
+                        <SelectValue placeholder="Pilih Kota" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="surabaya">Surabaya</SelectItem>
+                      <SelectItem value="malang">Malang</SelectItem>
+                      <SelectItem value="bali">Bali</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={profileForm.control}
+              name="tanggalLahir"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormControl>
+                    <Input
+                      className="px-4 py-6 outline outline-2 outline-blue-500 placeholder:text-[#898989]"
+                      type="date"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+
+        <FormField
+          control={profileForm.control}
+          name="noRm"
+          render={({ field }) => (
+            <FormItem className="mt-4">
+              <FormLabel className="font-semibold">No. RM</FormLabel>
+              <FormControl>
+                <Input
+                  className="px-4 py-6 outline outline-2 outline-blue-500 placeholder:text-[#898989]"
+                  placeholder="*00.20.xx - 99.xx.xx"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
       </form>
     </Form>
   );

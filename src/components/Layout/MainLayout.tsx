a9fda4ba71451555e0a0d355a7bbf4471f5f28a5
mrbananaaa <personal.astagiriii@gmail.com>
2024-04-27 16:29:48 +0700

    ui fix

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -4,8 +4,8 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div className="min-h-screen bg-slate-100">
-      <div className="font-main container px-4 py-6 pt-12">{children}</div>;
+    <div className="font-main min-h-screen bg-slate-100 antialiased">
+      <div className="container px-4 py-6 pt-12">{children}</div>;
     </div>
   );
 };

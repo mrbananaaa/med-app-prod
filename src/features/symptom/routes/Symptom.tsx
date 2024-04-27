f5b558d4e738777a97d40d914b4d164b0d0a0ce4
mrbananaaa <personal.astagiriii@gmail.com>
2024-04-27 14:39:50 +0700

    refactoring diagnosa button

--- src/features/symptom/routes/Symptom.tsx
+++ src/features/symptom/routes/Symptom.tsx
@@ -19,11 +19,13 @@ export const Symptom = () => {
 
       <SymptomOptions />
 
-      <Link to="/diagnosis">
-        <Button className="text-md mt-16 w-full bg-[#1939D2] py-7 font-semibold hover:bg-[#112FBD]">
-          Diagnosa
-        </Button>
-      </Link>
+      <div className="justify-center lg:flex">
+        <Link to="/diagnosis">
+          <Button className="text-md mt-16 w-full bg-[#1939D2] py-7 font-semibold hover:bg-[#112FBD] lg:w-fit lg:px-32">
+            Diagnosa
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };

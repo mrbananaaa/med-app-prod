3ce99090c78a9e0d82e14c7544255cd45f6df857
mrbananaaa <personal.astagiriii@gmail.com>
2024-04-28 12:15:44 +0700

    move ShowDiagnosisResult to separate component

---
+++ src/features/diagnosis/components/ShowDiagnosisResult.tsx
@@ -0,0 +1,20 @@
+export const ShowDiagnosisResult = () => {
+  return (
+    <div className="md:flex md:justify-center">
+      <div className="mt-14 font-semibold md:flex md:min-w-[400px] md:flex-col md:justify-center">
+        <div>
+          <h2>Diagnosa Penyakit</h2>
+          <div className="mt-2 rounded-md bg-[#1939D2] py-4 text-center text-sm text-white">
+            Tekanan Darah Tinggi (HIPERTENSI)
+          </div>
+        </div>
+        <div className="mt-2">
+          <h2>Tingkat Keparahan</h2>
+          <div className="mt-2 rounded-md bg-[#1939D2] py-4 text-center text-sm text-white">
+            Kronis
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/features/diagnosis/routes/Diagnosis.tsx
+++ src/features/diagnosis/routes/Diagnosis.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { VitalMonitor } from '@/features/misc';
 
+import { ShowDiagnosisResult } from '../components/ShowDiagnosisResult';
 import { ShowProfileForm } from '../components/ShowProfileForm';
 
 export const Diagnosis = () => {
@@ -32,22 +33,7 @@ export const Diagnosis = () => {
         </div>
       </div>
 
-      <div className="md:flex md:justify-center">
-        <div className="mt-14 font-semibold md:flex md:min-w-[400px] md:flex-col md:justify-center">
-          <div>
-            <h2>Diagnosa Penyakit</h2>
-            <div className="mt-2 rounded-md bg-[#1939D2] py-4 text-center text-sm text-white">
-              Tekanan Darah Tinggi (HIPERTENSI)
-            </div>
-          </div>
-          <div className="mt-2">
-            <h2>Tingkat Keparahan</h2>
-            <div className="mt-2 rounded-md bg-[#1939D2] py-4 text-center text-sm text-white">
-              Kronis
-            </div>
-          </div>
-        </div>
-      </div>
+      <ShowDiagnosisResult />
     </div>
   );
 };

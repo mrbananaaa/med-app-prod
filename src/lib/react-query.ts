340766280536524b56f490f4bb1d8e8ef1e9f676
mrbananaaa <personal.astagiriii@gmail.com>
2024-04-30 15:35:43 +0700

    setup error handling for query cache

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,3 +1,28 @@
-import { QueryClient } from '@tanstack/react-query';
+import { DefaultOptions, QueryCache, QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+import { toast } from '@/components/ui/use-toast';
+
+function errorToast(errorMessage: string) {
+  toast({
+    variant: 'destructive',
+    title: 'ERROR',
+    description: errorMessage ?? 'could not connecting to the server',
+  });
+}
+
+const queryConfig: DefaultOptions = {
+  queries: {
+    refetchOnWindowFocus: false,
+  },
+};
+
+const queryCache: QueryCache = new QueryCache({
+  onError: (error) => {
+    errorToast(error.message);
+  },
+});
+
+export const queryClient = new QueryClient({
+  defaultOptions: queryConfig,
+  queryCache: queryCache,
+});

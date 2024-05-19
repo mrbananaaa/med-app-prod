8aad7c8dcf621153faf79602da2750b620d96522
mrbananaaa <personal.astagiriii@gmail.com>
2024-05-19 21:24:42 +0700

    change data structure

--- src/features/misc/components/VitalMonitor.tsx
+++ src/features/misc/components/VitalMonitor.tsx
@@ -1,16 +1,20 @@
 export const VitalMonitor = () => {
-  const vitalList = ['Suhu', 'Denyut Nadi', 'Tekanan Darah'];
+  const data = [
+    ['Suhu', 'C'],
+    ['Denyut Nadi', '?'],
+    ['Tekanan Darah', '?'],
+  ] as const;
 
   return (
     <div className="mt-7 grid grid-cols-1 gap-y-2">
-      {vitalList.map((label) => (
+      {data.map(([label, unit]) => (
         <div
           key={label}
           className="flex items-center justify-between rounded-md bg-blue-500 p-3 font-medium"
         >
           <div className="text-sm font-semibold leading-3 text-white sm:text-base">{label}</div>
           <div className="flex items-center justify-center rounded-md bg-white px-4 py-2">
-            <div>200</div>
+            <div>200 {unit}</div>
           </div>
         </div>
       ))}
